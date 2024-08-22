cdfc6b6b3839e5332cb712bf41300588ff143b20
erikavilca <trabajoerika101@gmail.com>
2024-08-22 18:48:26 -0300

    entrega

--- tercerProyecto js/carrito.js
+++ tercerProyecto js/carrito.js
@@ -1,6 +1,5 @@
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [] ;
-// conosole.log(carrito)
-
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let containerPrecio = document.querySelector(`#containerPago`);
 
 const pintarProducto = (ArraysDeProducto) => {
   let containerCarrito = document.getElementById("containerProductos");
@@ -8,20 +7,98 @@ const pintarProducto = (ArraysDeProducto) => {
   ArraysDeProducto.forEach((producto) => {
     let productoCarrito = document.createElement("div");
     productoCarrito.className = "itemNuevos";
-    productoCarrito.innerHTML = `<img src=${producto.image}/>
-      <h2>${producto.nombre} </h2>
-      <p>${producto.precio}</p>
-      <p>${producto.quantity}</p>
+    productoCarrito.innerHTML = `<img src= "${producto.image}"/>
+    <spam class="boxInf">
+      <h2>${producto.title} </h2>
+      <p>${producto.price}</p>
+    <div class="cantidades">
+      <button onclick="restarProducto(${producto.id})">
+      -</button>
+      <spam>${producto.quantity}</spam>
+      <button onclick="sumarProducto(${producto.id})">
+      +</button>
+    </div>
       <button onclick="eliminarProducto(${producto.id})">
-      Eliminar</button>`;
+      Eliminar</button>
+    </spam>`;
     containerCarrito.appendChild(productoCarrito);
   });
 };
 pintarProducto(carrito);
 
-const eliminarProducto = (id) =>{
-    carrito = carrito.filter((producto) => producto.id !== id)
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    pintarProducto(carrito)
-}
+function totalAPagar() {
+  const precioFinal = carrito.reduce(
+    (acc, producto) => {
+      acc.precioTotal += producto.price * producto.quantity;
+      acc.cantidadTotal += producto.quantity;
+      return acc;
+    },
+    { precioTotal: 0, cantidadTotal: 0 }
+  );
+  if (precioFinal == 0) {
+    createElement;
+    // crear un div que diga que el carrito esta vacio
+  }
 
+  containerPrecio.innerHTML = "";
+  let boxPrecio = document.createElement(`div`);
+  boxPrecio.className = "Precio";
+  boxPrecio.innerHTML = `
+            <span> Productos (${precioFinal.cantidadTotal})</span><br>
+            <input type="text" id="validarCupon" placeholder="Escribir el cupon"> 
+            <button id="btn" onclick="obtenerValor()"> Enviar </button>
+            <p>Total $${Math.round(precioFinal.precioTotal)} </p>`;
+  containerPrecio.appendChild(boxPrecio);
+}
+totalAPagar();
+
+function obtenerValor() {
+  let cuponIngresado = document.getElementById("validarCupon");
+  let valor = cuponIngresado.value;
+  const unicoCupon = "Hol4";
+  if (valor == unicoCupon) {
+    let Toast= Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 4000,
+      timerProgressBar: true,
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Cupon valido"
+    });
+  }
+}
+const eliminarProducto = (id) => {
+  Swal.fire({
+    title: "¿Queres eliminar este producto del carrito?",
+    confirmButtonText: "confirmado",
+    showCancelButton: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carrito = carrito.filter((producto) => producto.id !== id);
+      totalAPagar();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      pintarProducto(carrito);
+    }
+  });
+};
+const sumarProducto = (id) => {
+  let Encontrado = carrito.find((elemento) => elemento.id === id);
+  if (Encontrado) {
+    Encontrado.quantity += 1;
+    totalAPagar();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    pintarProducto(carrito);
+  }
+};
+const restarProducto = (id) => {
+  let Encontrado = carrito.find((elemento) => elemento.id === id);
+  if (Encontrado && Encontrado.quantity > 1) {
+    Encontrado.quantity -= 1;
+    totalAPagar();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    pintarProducto(carrito);
+  }
+};
--- tercerProyecto js/main.js
+++ tercerProyecto js/main.js
@@ -1,90 +1,103 @@
-productos = [
-  {
-    nombre: "palo celeste",
-    categoria: "palos",
-    precio: "$100000",
-    id: 1,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "palo negro",
-    categoria: "palos",
-    precio: "$200000",
-    id: 2,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "palo de madera",
-    categoria: "palos",
-    precio: "$220000",
-    id: 3,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "palo vlack",
-    categoria: "palos",
-    precio: "$190000",
-    id: 4,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "palo jr",
-    categoria: "palos",
-    precio: "$150000",
-    id: 5,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "bolso negro",
-    categoria: "bolsos",
-    precio: "$90000",
-    id: 6,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "funda",
-    categoria: "bolsos",
-    precio: "$40000",
-    id: 7,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "bocha colores",
-    categoria: "accesorios",
-    precio: "$3000",
-    id: 8,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "bocha blanca",
-    categoria: "accesorios",
-    precio: "$3100",
-    id: 9,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "bocha profesional",
-    categoria: "accesorios",
-    precio: "$6000",
-    id: 10,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "canillera",
-    categoria: "accesorios",
-    precio: "$20000",
-    id: 11,
-    image: "https://via.placeholder.com/100",
-  },
-  {
-    nombre: "MediasPremiun",
-    categoria: "accesorios",
-    precio: "$2500",
-    id: 12,
-    image: "https://via.placeholder.com/100",
-  },
-];
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [] ; //se van agregado segun cunado producto elija el usuario.
+// productos = [
+//   {
+//     nombre: "palo celeste",
+//     categoria: "palos",
+//     precio: "100000",
+//     id: 1,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "palo negro",
+//     categoria: "palos",
+//     precio: "200000",
+//     id: 2,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "palo de madera",
+//     categoria: "palos",
+//     precio: "220000",
+//     id: 3,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "palo vlack",
+//     categoria: "palos",
+//     precio: "190000",
+//     id: 4,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "palo jr",
+//     categoria: "palos",
+//     precio: "150000",
+//     id: 5,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "bolso negro",
+//     categoria: "bolsos",
+//     precio: "90000",
+//     id: 6,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "funda",
+//     categoria: "bolsos",
+//     precio: "40000",
+//     id: 7,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "bocha colores",
+//     categoria: "accesorios",
+//     precio: "3000",
+//     id: 8,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "bocha blanca",
+//     categoria: "accesorios",
+//     precio: "3100",
+//     id: 9,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "bocha profesional",
+//     categoria: "accesorios",
+//     precio: "6000",
+//     id: 10,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "canillera",
+//     categoria: "accesorios",
+//     precio: "20000",
+//     id: 11,
+//     image: "https://via.placeholder.com/100",
+//   },
+//   {
+//     nombre: "MediasPremiun",
+//     categoria: "accesorios",
+//     precio: "2500",
+//     id: 12,
+//     image: "https://via.placeholder.com/100",
+//   },
+// ];
+let productos = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const getProduct = fetch("https://fakestoreapi.com/products");
+  getProduct
+    .then((res) => res.json())
+    .then((res) => {
+      productos = res;
+      pintarProducto(productos);
+      console.log(productos);
+    });
+});
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []; //se van agregado segun cunado producto elija el usuario.
 
 const pintarProducto = (ArraysDeProducto) => {
   let container = document.getElementById("container");
@@ -92,9 +105,10 @@ const pintarProducto = (ArraysDeProducto) => {
   ArraysDeProducto.forEach((producto) => {
     let productoCarrito = document.createElement("div");
     productoCarrito.className = "itemNuevos";
-    productoCarrito.innerHTML = `<img src=${producto.image}/>
-      <h2>${producto.nombre} </h2>
-      <p>${producto.precio}</p>
+    productoCarrito.innerHTML = `<img src= "${producto.image}"/>
+      <h2>${producto.title} </h2>
+      <span>${producto.price}</span><br>
+
       <button onclick="agregarAlCarrito(${producto.id})">
       agregar</button>`;
     container.appendChild(productoCarrito);
@@ -104,20 +118,14 @@ pintarProducto(productos);
 
 const agregarAlCarrito = (id) => {
   let producto = productos.find((elemento) => elemento.id === id);
-  let sumarProducto = carrito.find((elemento) => elemento.id === id);
+  let EncontradoEnCarrito = carrito.find((elemento) => elemento.id === id);
 
-  if(sumarProducto){
-    sumarProducto.quantity+=1 
-      localStorage.setItem("carrito", JSON.stringify(carrito));
-  }else{
-    carrito.push({...producto, quantity : 1});
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-      let mostrartotal= document.getElementById("resultado")
-  let otro= document.createElement("div")
-  otro.className= "costadoItem"
-  otro.innerHTML = `<p>${producto.nombre} ${producto.precio}</p>`
-  mostrartotal.appendChild(otro)
+  if (EncontradoEnCarrito) {
+    EncontradoEnCarrito.quantity += 1;
+  } else {
+    carrito.push({ ...producto, quantity: 1 });
+  }
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
 
 let inputBuscador = document.getElementById("buscador");
@@ -125,13 +133,13 @@ if (inputBuscador) {
   inputBuscador.addEventListener("input", (event) => {
     let value = event.target.value.toLowerCase();
     let filtroProducto = productos.filter((producto) =>
-      producto.nombre.toLowerCase().includes(value)
+      producto.title.toLowerCase().includes(value)
     );
     pintarProducto(filtroProducto);
   });
 }
 
-const formulario = document.getElementById("FiltradoFinal");
+const formulario = document.getElementById("Filtrado1");
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
   function obtenerSeleccion(nombre) {
@@ -144,12 +152,35 @@ formulario.addEventListener("submit", function (event) {
     });
     return seleccionado;
   }
-  let resultadoDeCategoria = obtenerSeleccion("categoria");
+  let resultadoDeCategoria = obtenerSeleccion("category");
   if (resultadoDeCategoria) {
     let final = productos.filter(
-      (producto) => producto.categoria == resultadoDeCategoria
+      (producto) => producto.category == resultadoDeCategoria
     );
     pintarProducto(final);
   }
 });
 
+const formulario2 = document.getElementById("Filtrado2");
+formulario2.addEventListener("submit", function (event) {
+  event.preventDefault();
+  function obtenerSeleccion(nombre) {
+    const radios = document.querySelectorAll(`input[name="${nombre}"]`);
+    let seleccionado;
+    radios.forEach((radio) => {
+      if (radio.checked) {
+        seleccionado = radio.value;
+      }
+    });
+    return seleccionado;
+  }
+  let resultadoDePrecio = obtenerSeleccion("price");
+  if (resultadoDePrecio == "60") {
+    let final = productos.filter((producto) => producto.price < 60);
+    pintarProducto(final);
+  } else if (resultadoDePrecio == "menorPrecio") {
+    pintarProducto(productos.sort((a, b) => a.price - b.price));
+  } else if (resultadoDePrecio == "mayorPrecio") {
+    pintarProducto(productos.sort((a, b) => b.price - a.price));
+  }
+});
